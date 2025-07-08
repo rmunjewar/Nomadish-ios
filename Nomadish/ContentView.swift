a7fceea975bf4008f04bb0060760d753f3dcb6a2
rmunjewar <riddhismunjewar@gmail.com>
2025-07-08 07:26:06 -0500

    added initial map base (set it to SFO for now)

--- Nomadish/ContentView.swift
+++ Nomadish/ContentView.swift
@@ -6,16 +6,20 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
+    @State private var position = MapCameraPosition.region(
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 37.7749, longitude: -122.4194), // SF
+            span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+        )
+    )
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Map(position: $position) {
         }
-        .padding()
+        .edgesIgnoringSafeArea(.all)
     }
 }
 
